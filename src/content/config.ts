9253853b77fcb9a49b773f4bc24d7a17f96f66c6
miaopan607 <2173679279@qq.com>
2025-12-27 14:37:27 +0800

    style: 统一代码格式

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,12 +1,12 @@
 import { defineCollection, z } from 'astro:content';
 
 const songs = defineCollection({
-	type: 'content',
-	schema: z.object({
-		title: z.string(),          // 歌名
-		releaseDate: z.string(),    // 发布日期
-		album: z.string(),          // 专辑
-	}),
+  type: 'content',
+  schema: z.object({
+    title: z.string(), // 歌名
+    releaseDate: z.string(), // 发布日期
+    album: z.string(), // 专辑
+  }),
 });
 
 export const collections = { songs };
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,11 +2,9 @@
 import typography from '@tailwindcss/typography';
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		typography,
-	],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [typography],
+};
