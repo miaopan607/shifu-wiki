dfc17100fd71f8203362ba9be53ab28a2c80c587
miaopan607 <2173679279@qq.com>
2025-12-27 20:34:01 +0800

    feat: 添加制作人员名单

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,6 +6,9 @@ const songs = defineCollection({
     title: z.string(), // 歌名
     releaseDate: z.string(), // 发布日期
     album: z.string(), // 专辑
+    lyricist: z.string(), // 词作
+    composer: z.string(), // 曲作
+    credits: z.string(), // 制作人员名单 (歌词样式)
   }),
 });
 
