c4f5357718f6b5f3205ffd413cfab070fd2f479c
miaopan607 <2173679279@qq.com>
2025-12-27 22:33:55 +0800

    使用4长度制表符缩进

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,15 @@
 import { defineCollection, z } from 'astro:content';
 
 const songs = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(), // 歌名
-    releaseDate: z.string(), // 发布日期
-    album: z.string(), // 专辑
-    lyricist: z.string(), // 词作
-    composer: z.string(), // 曲作
-    credits: z.string(), // 制作人员名单 (歌词样式)
-  }),
+	type: 'content',
+	schema: z.object({
+		title: z.string(), // 歌名
+		releaseDate: z.string(), // 发布日期
+		album: z.string(), // 专辑
+		lyricist: z.string(), // 词作
+		composer: z.string(), // 曲作
+		credits: z.string(), // 制作人员名单 (歌词样式)
+	}),
 });
 
 export const collections = { songs };
