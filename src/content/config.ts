249af0b79f522ac35a4d81c76bb0dacaf829d796
miaopan607 <2173679279@qq.com>
2025-12-28 11:19:13 +0800

    feat: 添加歌曲右侧边栏信息

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,6 +9,22 @@ const songs = defineCollection({
 		lyricist: z.string(), // 词作
 		composer: z.string(), // 曲作
 		credits: z.string(), // 制作人员名单 (歌词样式)
+		links: z
+			.array(
+				z.object({
+					name: z.string(),
+					url: z.string().url(),
+				}),
+			)
+			.optional(), // 相关链接
+		otherLinks: z
+			.array(
+				z.object({
+					name: z.string(),
+					url: z.string().url(),
+				}),
+			)
+			.optional(), // 其他链接
 	}),
 });
 
