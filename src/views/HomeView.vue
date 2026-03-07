cbe464324e3259bd8c03dbccdcd17b93ce8179ee
miaopan607 <2173679279@qq.com>
2026-03-07 20:26:18 +0800

    refactor(HomeView): 移除平台链接并重组导航菜单
    将哔哩哔哩、微博和网易云音乐的外部链接移除，并将个人介绍链接合并到主导航菜单中

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,9 +6,6 @@ const settings = {
 	home_subtitle: '开发预览',
 	home_slogan_1: '卿卿与黄',
 	home_slogan_2: '地久天长',
-	link_bilibili: 'https://space.bilibili.com/16330793',
-	link_weibo: 'https://weibo.com/u/2330621952',
-	link_music: 'https://music.163.com/#/artist?id=12308369',
 };
 </script>
 
@@ -42,20 +39,17 @@ const settings = {
 				<div class="h-0.5 w-24 bg-[#c9c9c9] shadow-[0_0_8px] shadow-[#c9c9c9]"></div>
 			</div>
 
-			<!-- 平台链接 -->
-			<nav class="flex justify-center gap-6 text-md font-light text-[#c9c9c9] text-lg">
-				<a :href="settings.link_bilibili" target="_blank" rel="noopener noreferrer" class="links">哔哩哔哩</a>
-				<a :href="settings.link_weibo" target="_blank" rel="noopener noreferrer" class="links">微博</a>
-				<a :href="settings.link_music" target="_blank" rel="noopener noreferrer" class="links">网易云音乐</a>
-				<RouterLink to="/profile" class="links">个人介绍</RouterLink>
-			</nav>
-
 			<!-- 导航 -->
 			<nav class="flex justify-center gap-6 text-md font-light text-[#c9c9c9] text-lg">
 				<RouterLink to="/songs" class="links">音乐</RouterLink>
 				<RouterLink to="/activities" class="links">活动</RouterLink>
 				<RouterLink to="/galleries" class="links">图集</RouterLink>
 				<RouterLink to="/misc" class="links">杂记</RouterLink>
+				<RouterLink to="/profile" class="links">个人介绍</RouterLink>
+			</nav>
+
+			<!-- 关于 -->
+			<nav class="flex justify-center gap-6 text-md font-light text-[#c9c9c9] text-lg">
 				<RouterLink to="/about" class="links">关于</RouterLink>
 			</nav>
 		</div>
